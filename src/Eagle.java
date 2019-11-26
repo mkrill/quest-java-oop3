2138d6f704d3d3a6c2d2b66ed4a532a54462e9e8
mkrill <michael.krill@telekom.de>
2019-11-26 12:33:03 +0100

    Changed descend to prevent reducing to an altitude < 0

--- src/Eagle.java
+++ src/Eagle.java
@@ -19,8 +19,10 @@ public class Eagle extends Bird implements Fly {
 
 	@Override
 	public int descend(int meters) {
-		this.altitude -= meters;
-		System.out.println(this.getName() + " flies downward, altitude: " + this.getAltitude());
+		if (this.getAltitude() > meters) {
+			this.altitude -= meters;
+			System.out.println(this.getName() + " flies downward, altitude: " + this.getAltitude());
+		}
 		return this.getAltitude();
 	}
 
@@ -30,7 +32,7 @@ public class Eagle extends Bird implements Fly {
 			System.out.println(this.getName() + " lands on the ground.");
 		} else {
 			System.out.println(this.getName() + " is too high, it can't land.");
-			
+
 		}
 	}
 
